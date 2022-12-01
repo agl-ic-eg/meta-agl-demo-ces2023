7897fb84d5d9aa539077a8dda570128579e81386
Hiroyuki Ishii <ishii.hiroyuki002@jp.panasonic.com>
2022-12-01 12:22:08 +0900

    Fix mkdisk.sh again to support both /dev/sd and /dev/mmcblk
    Signed-off-by: Hiroyuki Ishii <ishii.hiroyuki002@jp.panasonic.com>

--- mkdisk.sh
+++ mkdisk.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+[[ "$1" =~ ^/dev/sd ]] || partition_prefix=p
+
 sudo parted -s -a optimal $1 mklabel gpt
 sudo parted -s -a optimal $1 -- mkpart primary ext4 1 2GB
 sudo parted -s -a optimal $1 -- mkpart primary ext4 2GB 4GB
@@ -16,18 +18,18 @@ sudo parted -s -a optimal $1 -- mkpart primary ext4 26GB 27GB
 sudo parted -s -a optimal $1 -- mkpart primary ext4 27GB 28GB
 sudo parted -s -a optimal $1 -- mkpart primary ext4 28GB 29GB
 
-sudo mkfs.ext4 ${1}p1 -L host -F
-sudo mkfs.ext4 ${1}p2 -L agl-cluster -F
-sudo mkfs.ext4 ${1}p3 -L agl-momi-ivi -F
-sudo mkfs.ext4 ${1}p4 -L agl-qt-ivi -F
-sudo mkfs.ext4 ${1}p5 -L agl-flutter-ivi -F
-sudo mkfs.ext4 ${1}p6 -L agl-html5-ivi -F
-sudo mkfs.ext4 ${1}p7 -L agl-other -F
-sudo mkfs.ext4 ${1}p8 -L nvhost -F
-sudo mkfs.ext4 ${1}p9 -L nv-cluster -F
-sudo mkfs.ext4 ${1}p10 -L nv-momi-ivi -F
-sudo mkfs.ext4 ${1}p11 -L nv-qt-ivi -F
-sudo mkfs.ext4 ${1}p12 -L nv-flutter-ivi -F
-sudo mkfs.ext4 ${1}p13 -L nv-html5-ivi -F
-sudo mkfs.ext4 ${1}p14 -L nv-other -F
+sudo mkfs.ext4 ${1}${partition_prefix}1 -L host -F
+sudo mkfs.ext4 ${1}${partition_prefix}2 -L agl-cluster -F
+sudo mkfs.ext4 ${1}${partition_prefix}3 -L agl-momi-ivi -F
+sudo mkfs.ext4 ${1}${partition_prefix}4 -L agl-qt-ivi -F
+sudo mkfs.ext4 ${1}${partition_prefix}5 -L agl-flutter-ivi -F
+sudo mkfs.ext4 ${1}${partition_prefix}6 -L agl-html5-ivi -F
+sudo mkfs.ext4 ${1}${partition_prefix}7 -L agl-other -F
+sudo mkfs.ext4 ${1}${partition_prefix}8 -L nvhost -F
+sudo mkfs.ext4 ${1}${partition_prefix}9 -L nv-cluster -F
+sudo mkfs.ext4 ${1}${partition_prefix}10 -L nv-momi-ivi -F
+sudo mkfs.ext4 ${1}${partition_prefix}11 -L nv-qt-ivi -F
+sudo mkfs.ext4 ${1}${partition_prefix}12 -L nv-flutter-ivi -F
+sudo mkfs.ext4 ${1}${partition_prefix}13 -L nv-html5-ivi -F
+sudo mkfs.ext4 ${1}${partition_prefix}14 -L nv-other -F
 
